8155ce76d8f29ff3cd3f6587f3ce3f01faaa291d
gusepc <gustavofloresenciso@gmail.com>
2024-01-10 05:21:19 -0600

    des-2

--- des-1.js
+++ des-1.js
@@ -1,8 +1,36 @@
+  import * as fs from "fs"
+ 
+
 class ProductManager {
-    constructor(){
+
+  constructor(nombreDeArchivo){
+        this.path = nombreDeArchivo
         this.products = []
         this.productId = 0
+
+    
+     if (!fs.existsSync(this.path)) {
+        try {
+              fs.writeFileSync(this.path, "[]")
+        } catch (error) {
+            console.log("no se puedo crear el directorio" );
+        }
+    }  
+    else {
+            console.log("tu directorio ya existe");
+        }
+}   
+
+    addToDB() {
+      try {
+          const db = JSON.stringify(this.products);
+        fs.writeFileSync(this.path, db);
+          } catch (error) {
+             console.log("No se pudieron agregar tus productos");
+        }
     }
+   
+
     getProducts(){
         return this.products
     }
@@ -32,50 +60,84 @@ class ProductManager {
           this.products.push(newProduct)
           this.productId++
           console.log("se ha agredado un producto existosamente:", newProduct);
+
+          this.addToDB()
      
     }
-        getProductById(pId){
-            const product = this.products.find(product => product.id === pId);
-            if (product) {
-                console.log("Producto encontrado:", product);
-            } else {
-                console.log("Lo sentimos, el producto no existe")
-            }
+    updateProduct(pId, actualizacion) {
+        const i = this.products.findIndex(product => product.id === pId);
+            actualizacion.id = pId;
+            this.products[i] = actualizacion;
+            this.addToDB();
+            console.log("Producto ha sido actualizado:", actualizacion);
+    }
+
+    getProductById(pId){
+        const product = this.products.find(product => product.id === pId);
+        if (product) {
+            console.log("Producto encontrado:", product);
+        } else {
+            console.log("Lo sentimos, el producto no existe")
         }
+    }
+    deleteProduct(pId) {
+        let rango = this.products.length
+        const i = this.products.findIndex(p => p.id === pId);
+        if (i !== -1) {
+            const deletedProduct = this.products.splice(i, 1)[0];
+            this.addToDB();
+            console.log("Producto eliminado:", deletedProduct);
+
+        } else {
+            console.log("El producto no existe. No se puede eliminar.");
+        }
+    }
 }
 
 //testing
 
-/*
 
-const productManager = new ProductManager()
-console.log(productManager.getProducts());
-productManager.addProduct({title: 'producto prueba',
-    description:'Este es un producto prueba',
-    price:200,
-    thumbnail:'Sin imagen',
-    code:'abc123',
-    stock:25
-    })
-    productManager.addProduct({title: 'producto prueba',
-    description:'Este es un producto prueba',
-    price:200,
-    thumbnail:'Sin imagen',
-    code:'abc123',
-    stock:25
-    })
-    productManager.addProduct({title: 'producto prueba',
-    description:'Este es un producto prueba',
-    price:200,
-    thumbnail:'Sin imagen',
-    code:'abc1234',
-    stock:25
-    })
+// const productManager = new ProductManager("productos.json")
 
-console.log("productos existentes:", productManager.getProducts());
+// console.log(productManager.getProducts());
+// productManager.addProduct({title: 'producto prueba',
+//     description:'Este es un producto prueba',
+//     price:200,
+//     thumbnail:'Sin imagen',
+//     code:'abc123',
+//     stock:25
+//     })
+//     productManager.addProduct({title: 'producto prueba',
+//     description:'Este es un producto prueba',
+//     price:200,
+//     thumbnail:'Sin imagen',
+//     code:'abc123',
+//     stock:25
+//     })
+//     productManager.addProduct({title: 'producto prueba',
+//     description:'Este es un producto prueba',
+//     price:200,
+//     thumbnail:'Sin imagen',
+//     code:'abc1234',
+//     stock:25
+//     })
+
+// console.log("productos existentes:", productManager.getProducts());
 
 
-productManager.getProductById(2);
-productManager.getProductById(5);
+// productManager.getProductById(2);
+// productManager.getProductById(5);
 
-*/
+// const productoActualizado =
+// {title: 'producto actualizado',
+//     description:'Este es un producto actualizado',
+//     price:200,
+//     thumbnail:'Sin imagen',
+//     code:'abc1234',
+//     stock:25
+//     }
+
+// productManager.updateProduct(1, productoActualizado)
+
+// productManager.deleteProduct(2)
+// productManager.deleteProduct(3)
